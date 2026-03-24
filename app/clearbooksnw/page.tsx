49f2c0f39ad21dcabf39634eb87baeb39ed7f7c1
Xavier Finch <xfinch@mac.lan>
2026-03-23 21:32:53 -0700

    Add ClearBooks NW award site to public folder

--- app/clearbooksnw/page.tsx
+++ app/clearbooksnw/page.tsx
@@ -1,3 +1,10 @@
 export default function ClearBooksNW() {
-  return <div>Redirecting to award-winning site...</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-lg text-gray-600">Loading ClearBooks NW...</p>
+      <script dangerouslySetInnerHTML={{
+        __html: `window.location.href = '/clearbooksnw-award/';`
+      }} />
+    </div>
+  );
 }
